d52986be64f2024481017ad7f6ee92a5cca66636
cameron fuller <cameronmfuller@yahoo.com>
2017-12-13 18:14:32 -0500

    pseudo js

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+/*  Saw this style of pseudo online, found it appealing
+
+1. []   Setup globals: Time, Right Answers, Wrong Answers (maybe more)
+2. []   Setup array object pulpTrivia = [
+            question: "",
+            answers: ["","","",""],
+            right: ""]
+3. []   Setup timer (setInterval or setTimeout)
+4. []   Setup .click on answers array
+5. []   ++Right Answers
+6. []   ++Wrong Answers
+7. []   Show score
+8. []   Reset
+
+*/
+
